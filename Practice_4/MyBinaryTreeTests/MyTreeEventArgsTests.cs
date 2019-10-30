97545c77b8af5be059e044e8d4fc33792c3ecbdb
alexboliam <34817727+alexboliam@users.noreply.github.com>
2019-10-30 20:59:14 +0200

    Add 2 test classes and bin/debug

---
+++ Practice_4/MyBinaryTreeTests/MyTreeEventArgsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MyBinaryTree;
+
+namespace MyBinaryTreeTests
+{
+    public class MyTreeEventArgsTests
+    {
+        [Fact]
+        public void MyTreeEventArgs_CreateInstance_InstanceIsNotNull()
+        {
+            //Arrange
+            MyTreeEventArgs<string> args = null;
+            //Act
+            args = new MyTreeEventArgs<string>();
+            //Assert
+            Assert.NotNull(args);
+        }
+
+        [Fact]
+         public void MyTreeEventArgs_CreateInstanceWithValue_ValueInInstanceIsCorrect()
+         {
+            //Arrange
+            MyTreeEventArgs<string> args = null;
+            string value = "sample";
+            //Act
+            args = new MyTreeEventArgs<string>(value);
+            //Assert
+            Assert.Equal(value, args.Value);
+         }
+        
+    }
+}
--- Practice_4/MyBinaryTreeTests/NodeTests.cs
+++ Practice_4/MyBinaryTreeTests/NodeTests.cs
@@ -7,6 +7,40 @@ namespace MyBinaryTreeTests
     
     public class NodeTests
     {
-        
+        [Fact]
+        public void Node_CreateObject_ObjectIsNotNull()
+        {
+            //Arrange
+            Node<int> node = null;
+            //Act
+            node = new Node<int>();
+            //Assert
+            Assert.NotNull(node);
+        }
+
+        [Fact]
+        public void Node_CreateObjectWithValue_ObjectWithCorrectValue()
+        {
+            //Arrange
+            Node<int> node = null;
+            int value = 5;
+            //Act
+            node = new Node<int>(value);
+            //Assert
+            Assert.Equal(value, node.Data);
+        }
+
+        [Fact]
+        public void GetEnumerator_GetEnumeratorForNode_EnumeratorIsNotNull()
+        {
+            //Arrange
+            Node<int> node = new Node<int>(5);
+            node.Left = new Node<int>(2);
+            node.Right = new Node<int>(10);
+            //Act
+            var enumer = node.GetEnumerator();
+            //Assert
+            Assert.NotNull(enumer);
+        }
     }
 }
