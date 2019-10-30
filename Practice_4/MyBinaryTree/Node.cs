375d9b42d0a0922e7d8744dfff8655f89116bea4
alexboliam <34817727+alexboliam@users.noreply.github.com>
2019-10-30 20:43:38 +0200

    Add Practice 4: Unit testing empty project

---
+++ Practice_4/MyBinaryTree/Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBinaryTree
+{
+    public class Node<T> : IEnumerable<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node() { }
+        public Node(T Data)
+        {
+            this.Data = Data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (Left != null)
+            {
+                foreach (var v in Left)
+                {
+                    yield return v;
+                }
+            }
+
+            yield return Data;
+
+            if (Right != null)
+            {
+                foreach (var v in Right)
+                {
+                    yield return v;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            if (Left != null)
+            {
+                foreach (var v in Left)
+                {
+                    yield return v;
+                }
+            }
+
+            yield return Data;
+
+            if (Right != null)
+            {
+                foreach (var v in Right)
+                {
+                    yield return v;
+                }
+            }
+        }
+    }
+}
---
+++ Practice_4/MyBinaryTreeTests/NodeTests.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+using MyBinaryTree;
+
+namespace MyBinaryTreeTests
+{
+    
+    public class NodeTests
+    {
+        
+    }
+}
